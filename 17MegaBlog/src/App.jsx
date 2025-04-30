7407b03e3bddd2d94f98f0f359c67aeeb0c779f1
Tahirul Islam <5566tahirul@gmail.com>
2025-04-30 10:27:26 +0530

    Mega Blog Project

---
+++ 17MegaBlog/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);  
+  return (
+   <div>
+    <h1>Blog Application</h1>
+   </div>
+  )
+}
+export default App
