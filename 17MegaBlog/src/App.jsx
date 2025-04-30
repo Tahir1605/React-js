fa871a99cb0d8e27d32460e5cc7e84e6bb4982c3
Tahirul Islam <5566tahirul@gmail.com>
2025-04-30 18:33:38 +0530

    Mega Blog Project

--- 17MegaBlog/src/App.jsx
+++ 17MegaBlog/src/App.jsx
@@ -1,10 +1,34 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import {Header,Footer} from './components'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import { Outlet } from 'react-router-dom'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);  
-  return (
-   <div>
-    <h1>Blog Application</h1>
-   </div>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+  return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+     <div>
+      <Header/>
+      <main>
+      Todo   <Outlet/>
+      </main>
+      <Footer/>
+     </div>
+  </div>):null
 }
 export default App
