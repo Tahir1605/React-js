63c459807dd900949c675933e5764a2adb0ae9bf
Tahirul Islam <5566tahirul@gmail.com>
2025-05-01 13:08:10 +0530

    mega project

--- 17MegaBlog/src/App.jsx
+++ 17MegaBlog/src/App.jsx
@@ -1,34 +1,38 @@
-import { useEffect, useState } from 'react'
-import {Header,Footer} from './components'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import './App.css'
 import authService from "./appwrite/auth"
-import {login,logout} from "./store/authSlice"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
+
 function App() {
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
-  useEffect(()=>{
+  useEffect(() => {
     authService.getCurrentUser()
     .then((userData) => {
-      if(userData){
+      if (userData) {
         dispatch(login({userData}))
-      }
-      else{
+      } else {
         dispatch(logout())
       }
     })
     .finally(() => setLoading(false))
-  },[])
+  }, [])
+  
   return !loading ? (
-  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-     <div>
-      <Header/>
-      <main>
-      Todo   <Outlet/>
-      </main>
-      <Footer/>
-     </div>
-  </div>):null
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
+
 export default App
--- 17MegaBlog/src/appwrite/config.js
+++ 17MegaBlog/src/appwrite/config.js
@@ -1,24 +1,24 @@
-import conf from '../conf.js';
-import { Client,ID,Databases,Storage,Query } from 'appwrite';
+import conf from '../conf/conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Servive{
+export class Service{
     client = new Client();
     databases;
     bucket;
-
+    
     constructor(){
         this.client
-        .setEndpoint(conf.appWriteUrl)
-        .setProject(conf.appWriteProjectId);
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
     }
 
-    async createPost({title,slug,content,featuredImage,status,userId}){
-        try{
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
             return await this.databases.createDocument(
-                conf.appWriteDatabaseId,
-                conf.appWriteCollectionId,
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
                 slug,
                 {
                     title,
@@ -28,90 +28,85 @@ export class Servive{
                     userId,
                 }
             )
-
-        }catch(error){
-            console.log("Appwrite serve :: createPost :: error",error);
-            
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
-    async updatePost(slug,{title,content,featuredImage,status,userId}){
-        try{
-
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
             return await this.databases.updateDocument(
-                conf.appWriteDatabaseId,
-                conf.appWriteCollectionId,
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
                 slug,
                 {
                     title,
                     content,
                     featuredImage,
                     status,
+
                 }
             )
-
-        }catch (error){
-            console.log("Appwrite service :: updatePost :: error",error);
-            
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
     async deletePost(slug){
-           try{
+        try {
             await this.databases.deleteDocument(
-                conf.appWriteDatabaseId,
-                conf.appWriteCollectionId,
-                slug,
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
             )
             return true
-
-           }catch(error){
-            console.log("Appwrite service :: deletePost :: error",error);
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
             return false
-           }
+        }
     }
 
     async getPost(slug){
-        try{
-           return await this.databases.getDocument(
-            conf.appWriteDatabaseId,
-            conf.appWriteCollectionId,
-            slug,
-           )
-        }catch(error){
-            console.log("Appwrite serve :: getPost :: error",error);
-            return false
-        }
-    }
-
-    async getPosts(queries = [Query.equal("status","active")]){
         try {
-
-            return await this.databases.listDocuments(
-                conf.appWriteDatabaseId,
-                conf.appWriteCollectionId,
-                queries,
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            
             )
-            
         } catch (error) {
-            console.log("Appwrite service :: getPosts :: error", error);
+            console.log("Appwrite serive :: getPost :: error", error);
             return false
-            
         }
     }
 
-    // file Upload service
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+                
+
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    // file upload service
 
     async uploadFile(file){
         try {
             return await this.bucket.createFile(
-                conf.appWriteBucketId,
+                conf.appwriteBucketId,
                 ID.unique(),
-                file,
+                file
             )
-            
         } catch (error) {
-            console.log("Appwrite service :: uploadFile :: error", error);
+            console.log("Appwrite serive :: uploadFile :: error", error);
             return false
         }
     }
@@ -119,28 +114,24 @@ export class Servive{
     async deleteFile(fileId){
         try {
             await this.bucket.deleteFile(
-                conf.appWriteBucketId,
-                fileId,
+                conf.appwriteBucketId,
+                fileId
             )
             return true
-            
         } catch (error) {
-            console.log("Appwrite service :: deleteFile :: error",error);
+            console.log("Appwrite serive :: deleteFile :: error", error);
             return false
-            
         }
     }
 
     getFilePreview(fileId){
         return this.bucket.getFilePreview(
-            conf.appWriteBucketId,
-            field
+            conf.appwriteBucketId,
+            fileId
         )
     }
-
-    
-
 }
 
-const service = new Servive()
+
+const service = new Service()
 export default service
